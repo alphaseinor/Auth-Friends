eaea2505e67e8bbe0c849f4e31f725af1b83a6f9
Brian Hague <alphaseinor@gmail.com>
2019-12-11 03:37:35 -0600

    added changehandlers for age and string

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -1,41 +1,57 @@
 import React, {useState} from 'react'
 import axiosWithAuto from '../utils/axiosWithAuth.js'
+import {v1} from 'uuid'
 
 const AddFriend = () => {
-  const [values, setValues] = useState('')
+  const [friend, setFriend] = useState({
+    id:v1(),
+    name:'',
+    age:'',
+    email:''
+  })
 
-  const handleChange = event => {
-    event.persist()
-    setValues(values => ({ ...values, [event.target.name]: event.target.value }))
-  };
+  const handleChangeString = event => {
+    setFriend({...friend, [event.target.name]: event.target.value})
+  }
+
+  const handleChangeAge = event => {
+    setFriend({...friend, age: Number(event.target.value)})
+  }
+
+  const handleSubmit = event => {
+    
+  }
 
   return(
     <article
       className="friendCard"
     >
-      <form>
+      <form
+        onSubmit={e=>handleSubmit(e)}
+      >
         <h3>Add Friend</h3>
         <input
           type="text"
-          name=""
-          placeholder=""
-          value={''}
-          onChange = {handleChange}
+          name="name"
+          placeholder="Friend's Name"
+          value={friend.name}
+          onChange = {handleChangeString}
         />
         <input
           type="text"
-          name=""
-          placeholder=""
-          value={''}
-          onChange = {handleChange}
+          name="age"
+          placeholder="Friend's Age"
+          value={friend.age}
+          onChange = {handleChangeAge}
         />
         <input
           type="text"
-          name=""
-          placeholder=""
-          value={''}
-          onChange = {handleChange}
+          name="email"
+          placeholder="Friend's Email"
+          value={friend.email}
+          onChange = {handleChangeString}
         />
+        <button>Add Friend</button>
       </form>
     </article>
   )
