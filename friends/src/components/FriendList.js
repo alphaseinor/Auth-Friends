493fddb37558c8e63fcf52cc9944963b35d29dd0
Brian Hague <alphaseinor@gmail.com>
2019-12-10 20:10:18 -0600

    updated friendlist with a friends get request

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth.js'
 
 const FriendList = () => {
+  const [friend, setFriend] = useState([])
+ 
+  useEffect(()=>{
+    axiosWithAuth().get('http://localhost:5000/api/friends')
+      .then(result => {
+        console.log("friendslist", result)
+        setFriend(result.data.friends)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
   return (
-    <></>
+    <div>
+      {friend.map(person => (
+        person
+      ))}
+    </div>
   )
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
-const Login = () => {
+function Login () {
+  const [credentials, setCredentials] = useState 
+    ({
+        username: '',
+        password: ''
+    });
+  
+
+  const handleChange = event => {
+    setCredentials ({
+        ...credentials,
+        [event.target.name] : event.target.value
+      })
+    };
+
+  const onSubmit = event => {
+    event.preventDefault()
+
+    axiosWithAuth()
+    .post('/login', this.state.credentials)
+    .then (result => {
+      console.log('axios post', result.data.payload)
+      localStorage.setItem('token', result.data.payload)
+      this.props.history.push('/protected')
+    })
+    .catch(error => 
+      console.log(error))
+  }
+
   return (
-    <></>
+    <>
+    <form onSubmit = {onSubmit}>
+      <input 
+          type = 'text'
+          name = 'username'
+          placeholder = 'Username'
+          value = {credentials.username}
+          onChange = {handleChange}
+          />
+          <br></br>
+      <input 
+          type = 'password'
+          name = 'password'
+          placeholder = 'Password'
+          value = {credentials.password}
+          onChange = {handleChange}
+          />
+          <br></br>
+      <button> Log In </button>
+    </form>
+  </>
   )
 }
 
